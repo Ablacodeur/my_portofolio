aa1f975974808ff3908041a03c1fd6a3c81e9699
Ablacodeur <agbolivicky@gmail.com>
2025-12-21 02:58:39 -0500

    footer and works

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,202 +1,25 @@
 import { Box, Button, Stack, Typography } from '@mui/material'
 import React from 'react'
-import me_img from '../../assets/resources/me.png'
+import me_img from '../../assets/resources/footerImage.png'
 
 
 export default function Contact() {
   return (
-    <>
-      {/* 📱 MOBILE : texte → image → boutons */}
-      <Stack
-        sx={{
-          display: { xs: 'flex', md: 'none' },
-          flexDirection: 'column',
-          minHeight: '100vh',
-          gap: 3,
-          alignItems: 'center'
-        }}
-      >
-        {/* 1️⃣ TEXTE */}
-        <Box sx={{ width: '100%', textAlign: 'start', mt: 3, px: 2 }}>
-          <Typography variant='h3' sx={{ fontFamily: 'inter', fontWeight: '900' }}>
-            Nice to meet
-          </Typography>
-          <Typography sx={{ color: '#777272', fontWeight: '400', mt: 3}}>
-            Located in Canada. Online - near you <br />
-          </Typography>
+    <Box sx={{ height:{xs:'60vh',md:'100vh'},margin:'10px',position:'absolute',display:'flex',alignItems:'center',justifyContent:'center',flexDirection:'column',width:'100vw',padding:{xs:'15px',md:'30px',overflow:'hidden'} }}>
+      <Typography variant='h1' sx={{ fontSize:{xs:'100px',md:'300px'} ,color:'#ff008b',fontFamily:'Corinthia',textAlign:'center',display:'flex',zIndex:'2',position:'relative'}}>Nice to meet</Typography>
+      <Box sx={{ display:'flex',justifyContent:'center',bottom:{xs:'70px',md:'150px'},position:'relative',height:{xs:'30vh',md:'60vh'},alignItems:'center',width:{xs:'250px',md:'550px'}}}>
+        <img src={me_img} style={{ height:'100%',width:'100%'}} />
+      </Box>
+      <Box sx={{textAlign:'center', alignItems:'center', display:'flex',justifyContent:'space-between',flexDirection:'row',backgroundColor:'black',padding:'5px',color:'white',width:'100vw',height:'15vh',overflow:'hidden'}}>
+        <Typography sx={{ fontSize:{xs:'15px',md:'23px'} }}>Linkedin</Typography>
+        <Typography sx={{ fontSize:{xs:'15px',md:'23px'} }}>Github</Typography>
+        <Typography sx={{ fontSize:{xs:'15px',md:'23px'} }}> Email</Typography>
+        <Typography sx={{ fontSize:{xs:'15px',md:'23px'} }}>Contact</Typography>
+      </Box>
+        <Box sx={{padding:'20px',width:'100vw',textAlign:'center'}}>
+        <Typography sx={{ fontSize:{xs:'12px',md:'20px'} }}>Located in Canada</Typography>
+      </Box>
 
-        {/* 3️⃣ BUTTONS */}
-        <Box sx={{ width: '100%',px: 2, mb: 4 ,
-                display:'flex',flexDirection:'column',alignItems:'flex-start'}}>
-            <Button variant="text" sx={{ padding: 0, mt: 2 }}>
-            <Typography
-                sx={{
-                textDecoration: 'underline',
-                color: 'black',
-                fontSize: '35px',
-                fontFamily: 'inter',
-                fontWeight: '600',
-                display: 'flex',
-                alignItems: 'center',
-                gap: '8px', 
-                textTransform: 'none',
-                }}
-            >
-                Craft like this
-
-                <Box
-                component="span"
-                sx={{
-                    display: 'inline-flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    width: '1.05em',   
-                    height: '1.8em', 
-                    
-                }}
-                >
-                <svg
-                    viewBox="0 0 24 24"
-                    width="100%"
-                    height="100%"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path
-                    d="M2 8h2v2h2v2h2v2h2v-2h2v-2h2v2h2v2h2v2h-4v2h8v-8h-2v4h-2v-2h-2v-2h-2V8h-2v2h-2v2H8v-2H6V8H4V6H2v2z"
-                    fill="#000000"
-                    />
-                </svg>
-                </Box>
-            </Typography>
-            </Button>
-
-        </Box>
-
-        </Box>
-
-        {/* 2️⃣ IMAGE */}
-        <Box sx={{ width: '100%', px: 2, }}>
-          <img
-            src={me_img}
-            style={{ width: '100%', height: 'auto', objectFit: 'cover',height:'40vh' }}
-          />
-        </Box>
-
-      </Stack>
-
-      {/* 🖥 DESKTOP : [ image ] [ texte + boutons ] */}
-      <Stack
-        sx={{
-          display: { xs: 'none', md: 'flex' },
-          height: '100vh',
-          flexDirection: 'row',
-          justifyContent: 'space-around',
-          alignItems: 'center'
-
-        }}
-      >
-        {/* IMAGE GAUCHE */}
-        <Box sx={{ height: '100%' }}>
-          <img
-            src={me_img}
-            style={{ width: '100%', height: '90vh', objectFit: 'cover' }}
-          />
-        </Box>
-
-        {/* COLONNE TEXTE + BOUTONS */}
-        <Box
-          sx={{
-            height: '80%',
-            textAlign: 'start',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center'
-          }}
-        >
-          <Typography variant='h3' sx={{ fontFamily: 'inter', fontWeight: '900' }}>
-            Full stack web Developer <br />
-          </Typography>
-          <Box sx={{ alignItems:'end',justifyContent:'end',textAlign:'end',display:'flex',}}>
-            <Typography sx={{ fontSize:'1em',fontWeight:'200',padding:'3px',
-            backgroundColor:'#ff008b',color:'white',width:'23%'}}>
-            Base in Montreal
-            </Typography>
-          </Box>
-
-          <Typography
-            sx={{
-              color: '#777272',
-              fontWeight: '400',
-              mt: 5
-            }}
-          >
-            I'm Abla Victoire Agboli. <br />
-            I create web experiences that hit hard and stand out.
-          </Typography>
-
-            <Box sx={{ width: '100%',px: 2, mb: 4 ,
-                display:'flex',flexDirection:'column',alignItems:'flex-start',textAlign:'start',padding:'0'}}
-            >
-
-            <Button variant="text" sx={{ padding: 0, mt: 2 }}>
-            <Typography
-                sx={{
-                textDecoration: 'underline',
-                color: 'black',
-                fontSize: '40px',
-                fontFamily: 'inter',
-                fontWeight: '600',
-                display: 'flex',
-                alignItems: 'center',
-                gap: '8px', 
-                textTransform: 'none',
-                }}
-            >
-                Craft like this
-
-                <Box
-                component="span"
-                sx={{
-                    display: 'inline-flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    width: '1.1em',   
-                    height: '1.1em', 
-                }}
-                >
-                <svg
-                    viewBox="0 0 24 24"
-                    width="100%"
-                    height="100%"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path
-                    d="M2 8h2v2h2v2h2v2h2v-2h2v-2h2v2h2v2h2v2h-4v2h8v-8h-2v4h-2v-2h-2v-2h-2V8h-2v2h-2v2H8v-2H6V8H4V6H2v2z"
-                    fill="#000000"
-                    />
-                </svg>
-                </Box>
-            </Typography>
-            </Button>
-
-            <Button variant="text" sx={{ padding: 0, mt: 2 }}>
-              <Typography
-                sx={{
-                  color: '#ff008b',
-                  fontSize: '37px',
-                  fontFamily: 'inter',
-                  fontWeight: '700',
-                  textTransform: 'none',
-                }}
-              >
-                Portfolio
-              </Typography>
-            </Button>
-          </Box>
-        </Box>
-      </Stack>
-    </>
+    </Box>
   )
 }
--- src/Components/Works/Works.jsx
+++ src/Components/Works/Works.jsx
@@ -1,92 +1,204 @@
 import { Box, Button, Stack, Typography } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import workdemo1 from '../../assets/resources/workSlide1.png'
 import workdemo2 from '../../assets/resources/workSlide2.png'
-
-
 import 'swiper/css';
 import 'swiper/css/pagination';
-
-
 // Modules
 import { Pagination, Autoplay } from 'swiper/modules';
-import { Link, Navigate, useNavigate } from 'react-router-dom';
 
 export default function Works() {
+   const [openSwiper, setOpenSwiper] = useState(null);
+
+
   return (
-    <Box sx={{ height:'100vh',padding:'55px' }}>
+    <Box sx={{ height:'',padding:'55px' }}>
         <Typography variant="h1"sx={{ fontSize:{xs:'70px',md:'155px'},fontWeight:'550'}}>Works.</Typography>
-        <Stack sx={{ height:'70vh',justifyContent:'space-around'}}>
+        <Stack sx={{ height:'',justifyContent:'space-around'}}>
             <Box sx={{ display:'flex',justifyContent:'space-between',flexDirection:'column'  }}>
                 <Box sx={{ display:'flex',justifyContent:'space-between',height:'15%',alignContent:'' }}>
-                    <Typography sx={{ fontSize:'60px',fontFamily:'Great Vibes',padding:'0' }}>001</Typography>
+                    <Typography sx={{ fontSize:{xs:'30px',md:'60px'},fontFamily:'Great Vibes',padding:'0' }}>001</Typography>
                     <Box sx={{ padding:'0',display:'flex',flexDirection:'column',alignItems:'start',width:'40%' ,marginBottom:'50px'}}>
-                        <Typography sx={{ fontSize:'60px'}}>Backend</Typography>
+                        <Typography sx={{ fontSize:{xs:'25px',md:'60px'}}}>Backend</Typography>
                         <Typography>Id qui aliquip eiusmod est sint laboris et veniam.</Typography>
-                        <Button sx={{ padding:'0' }}>Explore</Button>
+                            <Button
+                            sx={{ padding: '0' }}
+                            onClick={() =>
+                                setOpenSwiper(openSwiper === 1 ? null : 1)
+                            }
+                            >
+                            Explore
+                            </Button>
+
                     </Box>
                 </Box>
                 <hr />
+                {openSwiper===1 && (
+                    <Box sx={{ display:'flex',justifyContent:'space-between',height:'50vh',marginTop:'20px'}}>
+                        
+                        <Box sx={{ padding:'0',display:'flex',flexDirection:'column',width:'50%' }}>
+                            <Swiper
+                                direction="vertical"
+                                modules={[Autoplay]}
+                                pagination={{ clickable: true }}
+                                autoplay={{
+                                    delay: 500,
+                                    disableOnInteraction: false, // continue même si le user touche
+                                }}
+                                speed={600} // fluidité de la transition
+                                loop={true} // boucle infinie important
+                                className="mySwiper"
+                                >
+                                <SwiperSlide><img style={{ width:'100%',height:"auto" ,overflow:'hidden',objectFit:'contain'}} src={workdemo1} /></SwiperSlide>
+                                <SwiperSlide><img style={{ width:'100%',height:"auto",overflow:'hidden',objectFit:'contain' }} src={workdemo2} /></SwiperSlide>
+                            </Swiper>                    
+                        </Box>
+                        <Box sx={{ width:'40%',display:'flex',justifyContent:'space-between',flexDirection:'column'}}>
+                            <Box>
+                            <Typography variant='h6' sx={{ fontSize:'20px',fontFamily:'inter',padding:'0'}}>Id qui aliquip eiusmod est .</Typography>
+                            <Typography variant='h7' sx={{ fontSize:'17px',fontFamily:'inter',padding:'0' }}>Esse o nulla est in eniempor ipsum minim eu adipisicing dolore pariatur commodo labore occaecat qui.</Typography>
+                            </Box>
+                            <Box>
+                            <Typography variant='h6' sx={{ fontSize:'20px',fontFamily:'inter',padding:'0'}}>Id qui aliquip eiusmod est .</Typography>
+                            <Typography variant='h7' sx={{ fontSize:'17px',fontFamily:'inter',padding:'0' }}>Esse o nulla est in eniempor ipsum minim eu adipisicing dolore pariatur commodo labore occaecat qui.</Typography>
+                            </Box>
+                            <Box>
+                            <Typography variant='h6' sx={{ fontSize:'20px',fontFamily:'inter',padding:'0'}}>Id qui aliquip eiusmod est .</Typography>
+                            <Typography variant='h7' sx={{ fontSize:'17px',fontFamily:'inter',padding:'0' }}>Esse o nulla est in eniempor ipsum minim eu adipisicing dolore pariatur commodo labore occaecat qui.</Typography>
+                            </Box>
+                            <Box >
+                            <Typography  variant='h6' sx={{ fontSize:'20px',fontFamily:'inter',padding:'0'}}  >  Visite the site   </Typography>
+                            </Box>
 
-                <Box sx={{ display:'flex',justifyContent:'space-between',height:'50vh',marginTop:'20px'}}>
-                    
-                    <Box sx={{ padding:'0',display:'flex',flexDirection:'column',width:'50%' }}>
-                        <Swiper
-                            direction="vertical"
-                            modules={[Autoplay]}
-                            pagination={{ clickable: true }}
-                            autoplay={{
-                                delay: 500,
-                                disableOnInteraction: false, // continue même si le user touche
-                            }}
-                            speed={600} // fluidité de la transition
-                            loop={true} // boucle infinie important
-                            className="mySwiper"
+
+                        </Box>
+                    </Box>
+                )}
+            </Box>
+            {openSwiper && ( <hr />)}
+           
+            
+            <Box sx={{ display:'flex',justifyContent:'space-between',flexDirection:'column'  }}>
+                <Box sx={{ display:'flex',justifyContent:'space-between',height:'15%',alignContent:'' }}>
+                    <Typography sx={{ fontSize:{xs:'30px',md:'60px'},fontFamily:'Great Vibes',padding:'0' }}>002</Typography>
+                    <Box sx={{ padding:'0',display:'flex',flexDirection:'column',alignItems:'start',width:'40%' ,marginBottom:'50px'}}>
+                        <Typography sx={{ fontSize:{xs:'25px',md:'60px'}}}>Frontend</Typography>
+                        <Typography>Id qui aliquip eiusmod est sint laboris et veniam.</Typography>
+                            <Button
+                            sx={{ padding: '0' }}
+                            onClick={() =>
+                                setOpenSwiper(openSwiper === 2 ? null : 2)
+                            }
                             >
-                            <SwiperSlide><img style={{ width:'100%',height:"auto" ,overflow:'hidden',objectFit:'contain'}} src={workdemo1} /></SwiperSlide>
-                            <SwiperSlide><img style={{ width:'100%',height:"auto",overflow:'hidden',objectFit:'contain' }} src={workdemo2} /></SwiperSlide>
-                        </Swiper>                    
-                    </Box>
-                    <Box sx={{ width:'40%',display:'flex',justifyContent:'space-between',flexDirection:'column'}}>
-                        <Box>
-                        <Typography variant='h6' sx={{ fontSize:'20px',fontFamily:'inter',padding:'0'}}>Id qui aliquip eiusmod est .</Typography>
-                        <Typography variant='h7' sx={{ fontSize:'17px',fontFamily:'inter',padding:'0' }}>Esse o nulla est in eniempor ipsum minim eu adipisicing dolore pariatur commodo labore occaecat qui.</Typography>
-                        </Box>
-                        <Box>
-                        <Typography variant='h6' sx={{ fontSize:'20px',fontFamily:'inter',padding:'0'}}>Id qui aliquip eiusmod est .</Typography>
-                        <Typography variant='h7' sx={{ fontSize:'17px',fontFamily:'inter',padding:'0' }}>Esse o nulla est in eniempor ipsum minim eu adipisicing dolore pariatur commodo labore occaecat qui.</Typography>
-                        </Box>
-                        <Box>
-                        <Typography variant='h6' sx={{ fontSize:'20px',fontFamily:'inter',padding:'0'}}>Id qui aliquip eiusmod est .</Typography>
-                        <Typography variant='h7' sx={{ fontSize:'17px',fontFamily:'inter',padding:'0' }}>Esse o nulla est in eniempor ipsum minim eu adipisicing dolore pariatur commodo labore occaecat qui.</Typography>
-                        </Box>
-                        <Box >
-                        <Typography  variant='h6' sx={{ fontSize:'20px',fontFamily:'inter',padding:'0'}}  >  Visite the site   </Typography>
-                        </Box>
-
+                            Explore
+                            </Button>
 
                     </Box>
                 </Box>
+                <hr />
+                {openSwiper===3 && (
+                    <Box sx={{ display:'flex',justifyContent:'space-between',height:'50vh',marginTop:'20px'}}>
+                        
+                        <Box sx={{ padding:'0',display:'flex',flexDirection:'column',width:'50%' }}>
+                            <Swiper
+                                direction="vertical"
+                                modules={[Autoplay]}
+                                pagination={{ clickable: true }}
+                                autoplay={{
+                                    delay: 500,
+                                    disableOnInteraction: false, // continue même si le user touche
+                                }}
+                                speed={600} // fluidité de la transition
+                                loop={true} // boucle infinie important
+                                className="mySwiper"
+                                >
+                                <SwiperSlide><img style={{ width:'100%',height:"auto" ,overflow:'hidden',objectFit:'contain'}} src={workdemo1} /></SwiperSlide>
+                                <SwiperSlide><img style={{ width:'100%',height:"auto",overflow:'hidden',objectFit:'contain' }} src={workdemo2} /></SwiperSlide>
+                            </Swiper>                    
+                        </Box>
+                        <Box sx={{ width:'40%',display:'flex',justifyContent:'space-between',flexDirection:'column'}}>
+                            <Box>
+                            <Typography variant='h6' sx={{ fontSize:'20px',fontFamily:'inter',padding:'0'}}>Id qui aliquip eiusmod est .</Typography>
+                            <Typography variant='h7' sx={{ fontSize:'17px',fontFamily:'inter',padding:'0' }}>Esse o nulla est in eniempor ipsum minim eu adipisicing dolore pariatur commodo labore occaecat qui.</Typography>
+                            </Box>
+                            <Box>
+                            <Typography variant='h6' sx={{ fontSize:'20px',fontFamily:'inter',padding:'0'}}>Id qui aliquip eiusmod est .</Typography>
+                            <Typography variant='h7' sx={{ fontSize:'17px',fontFamily:'inter',padding:'0' }}>Esse o nulla est in eniempor ipsum minim eu adipisicing dolore pariatur commodo labore occaecat qui.</Typography>
+                            </Box>
+                            <Box>
+                            <Typography variant='h6' sx={{ fontSize:'20px',fontFamily:'inter',padding:'0'}}>Id qui aliquip eiusmod est .</Typography>
+                            <Typography variant='h7' sx={{ fontSize:'17px',fontFamily:'inter',padding:'0' }}>Esse o nulla est in eniempor ipsum minim eu adipisicing dolore pariatur commodo labore occaecat qui.</Typography>
+                            </Box>
+                            <Box >
+                            <Typography  variant='h6' sx={{ fontSize:'20px',fontFamily:'inter',padding:'0'}}  >  Visite the site   </Typography>
+                            </Box>
 
+
+                        </Box>
+                    </Box>
+                )}
             </Box>
-            <hr />
-            <Box sx={{ display:'flex',justifyContent:'space-between',height:'15%' }}>
-                <Typography sx={{ fontSize:'60px',fontFamily:'Great Vibes' }}>002</Typography>
-                <Box sx={{ padding:'0',display:'flex',flexDirection:'column',alignItems:'flex-end' }}>
-                    <Typography sx={{ fontSize:'60px'}}>FullStack</Typography>
-                    <Typography>Id qui aliquip eiusmod est sint laboris et veniam.</Typography>
-                    <Button sx={{ padding:'0' }}>Explore</Button>
+            {openSwiper && ( <hr />)}
+            <Box sx={{ display:'flex',justifyContent:'space-between',flexDirection:'column'  }}>
+                <Box sx={{ display:'flex',justifyContent:'space-between',height:'15%',alignContent:'' }}>
+                    <Typography sx={{ fontSize:{xs:'30px',md:'60px'},fontFamily:'Great Vibes',padding:'0' }}>003</Typography>
+                    <Box sx={{ padding:'0',display:'flex',flexDirection:'column',alignItems:'start',width:'40%' ,marginBottom:'50px'}}>
+                        <Typography sx={{ fontSize:{xs:'25px',md:'60px'}}}>Comming project</Typography>
+                        <Typography>Id qui aliquip eiusmod est sint laboris et veniam.</Typography>
+                            <Button
+                            sx={{ padding: '0' }}
+                            onClick={() =>
+                                setOpenSwiper(openSwiper === 3 ? null : 3)
+                            }
+                            >
+                            Explore
+                            </Button>
+
+                    </Box>
                 </Box>
-            </Box>
-             <hr />
-            <Box sx={{ display:'flex',justifyContent:'space-between',height:'15%' }}>
-                <Typography sx={{ fontSize:'60px',fontFamily:'Great Vibes' }}>003</Typography>
-                <Box sx={{ padding:'0',display:'flex',flexDirection:'column',alignItems:'flex-end' }}>
-                    <Typography sx={{ fontSize:'60px'}}>Coming project</Typography>
-                    <Typography>Id qui aliquip eiusmod est sint laboris et veniam.</Typography>
-                    <Button sx={{ padding:'0' }}>Explore</Button>
-                </Box>
+                <hr />
+                {openSwiper===3 && (
+                    <Box sx={{ display:'flex',justifyContent:'space-between',height:'50vh',marginTop:'20px'}}>
+                        
+                        <Box sx={{ padding:'0',display:'flex',flexDirection:'column',width:'50%' }}>
+                            <Swiper
+                                direction="vertical"
+                                modules={[Autoplay]}
+                                pagination={{ clickable: true }}
+                                autoplay={{
+                                    delay: 500,
+                                    disableOnInteraction: false, // continue même si le user touche
+                                }}
+                                speed={600} 
+                                loop={true} // boucle infinie important
+                                className="mySwiper"
+                                >
+                                <SwiperSlide><img style={{ width:'100%',height:"auto" ,overflow:'hidden',objectFit:'contain'}} src={workdemo1} /></SwiperSlide>
+                                <SwiperSlide><img style={{ width:'100%',height:"auto",overflow:'hidden',objectFit:'contain' }} src={workdemo2} /></SwiperSlide>
+                            </Swiper>                    
+                        </Box>
+                        <Box sx={{ width:'40%',display:'flex',justifyContent:'space-between',flexDirection:'column'}}>
+                            <Box>
+                            <Typography variant='h6' sx={{ fontSize:'20px',fontFamily:'inter',padding:'0'}}>Id qui aliquip eiusmod est .</Typography>
+                            <Typography variant='h7' sx={{ fontSize:'17px',fontFamily:'inter',padding:'0' }}>Esse o nulla est in eniempor ipsum minim eu adipisicing dolore pariatur commodo labore occaecat qui.</Typography>
+                            </Box>
+                            <Box>
+                            <Typography variant='h6' sx={{ fontSize:'20px',fontFamily:'inter',padding:'0'}}>Id qui aliquip eiusmod est .</Typography>
+                            <Typography variant='h7' sx={{ fontSize:'17px',fontFamily:'inter',padding:'0' }}>Esse o nulla est in eniempor ipsum minim eu adipisicing dolore pariatur commodo labore occaecat qui.</Typography>
+                            </Box>
+                            <Box>
+                            <Typography variant='h6' sx={{ fontSize:'20px',fontFamily:'inter',padding:'0'}}>Id qui aliquip eiusmod est .</Typography>
+                            <Typography variant='h7' sx={{ fontSize:'17px',fontFamily:'inter',padding:'0' }}>Esse o nulla est in eniempor ipsum minim eu adipisicing dolore pariatur commodo labore occaecat qui.</Typography>
+                            </Box>
+                            <Box >
+                            <Typography  variant='h6' sx={{ fontSize:'20px',fontFamily:'inter',padding:'0'}}  >  Visite the site   </Typography>
+                            </Box>
+
+
+                        </Box>
+                    </Box>
+                )}
             </Box>
         </Stack>
         
